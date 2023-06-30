2f48dc411391623f7846b06b580e13fdfe67cb5e
Mwangi, Anthony Benjamin Ngaru A <mwangia@stanbic.com>
2023-06-30 16:34:32 +0300

    creating services and dependancy injections

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({ // decorator
   selector: 'pm-products',
@@ -7,7 +8,10 @@ import { IProduct } from "./product";
   styleUrls:['./product-list.component.css'] // Encapsulating component styles
 })
 
-export class ProductListComponent implements OnInit{ 
+export class ProductListComponent implements OnInit{
+
+  // Inject the ProductService during class construction
+  constructor(private productService: ProductService){}
 
   // Declaire all your properties
   pageTitle: string = "Product List";
@@ -30,58 +34,7 @@ export class ProductListComponent implements OnInit{
 
   filteredProducts: IProduct[] = [];
   // utilize an interface
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2021",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2020",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
-  ];
+  products: IProduct[] = [];
 
   // Declaire your methods (logic / functions)
   toggleImage(): void {
@@ -89,11 +42,8 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this._listFilter = 'cart';
-    this.filteredProducts = this.performFilter(this._listFilter);
-    console.log('In OnInit ', this._listFilter);
-    console.log(this.filteredProducts);
-    
+    this.products = this.productService.getProducts() // get products from Products Services
+    this.filteredProducts = this.products // push products to filtered products
   }
 
   performFilter(filterBy:string): IProduct[]{
