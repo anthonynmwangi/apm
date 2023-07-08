f897b3e8b7913b4d0ae69acb6068c11c249c06ae
Anthony Mwangi <anthonynmwangi@gmail.com>
2023-07-08 23:19:38 +0300

    Create routes for diff pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   imports: [ 
     // introduce external moduels
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch:'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch:'full' }
+    ])
   ],
   declarations: [
+    // include all components/pipes here
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   bootstrap: [AppComponent]
 })
